6b7c55e7e1e97d295bd47808d8d60d230e7b16ae
ericalundstrom <erica.lundstrom99@hotmail.com>
2023-03-30 13:39:29 +0200

    Update login.php

--- api/login.php
+++ api/login.php
@@ -33,7 +33,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     $message = ["message" => "Not found!"];
     sendJSON($message, 404);
-    }
-
+    
+}
+$message = ["message" => "Wrong kind of Request Method."];
+sendJSON($message, 404);
+    
 
 ?>
